ce004fe9f9fd1aede212e25fe8ae43a42bcea457
Sebastião Neto <sebastiao@landix.com.br>
2020-06-29 21:53:58 +0100

    Inspeção 29/06

--- server/frigobar/serializers.py
+++ server/frigobar/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Produto
 
-class ProdutoSerializer(serializers.ModelSerializer):
+class ProdutoSerializer(serializers.ModelSerializer):  # TODO Se atentar às convenções para organização do código (nesse caso duas linhas em branco antes da classe)
     class Meta:
         model = Produto
         depth = 0
