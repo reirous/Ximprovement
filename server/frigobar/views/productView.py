549e47eeeb3411d7aa1c80eb12b3b5d18655b293
reirous <luanof@hotmail.com>
2020-07-27 00:38:41 -0300

    Migração de requisições do backend antigo para modelo de Produtos

--- server/frigobar/views/productView.py
+++ server/frigobar/views/productView.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins  import UpdateModelMixin
 from frigobar.models.product import Product
 from frigobar.serializers.productSerializer import ProductSerializer
 # Create your views here.
 
 class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('description')
+    serializer_class = ProductSerializer
+
+class ProductGetActiveView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.filter(status=True)
+
+class ProductPartialUpdateView(GenericAPIView, UpdateModelMixin):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    # Allows to edit chosen fields
+    def put(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
