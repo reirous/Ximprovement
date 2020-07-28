a58476fab40968b9f5f72c73176e88088d17b084
caiocezarq <caio_queiroz09@hotmail.com>
2020-07-28 00:52:19 -0300

    PartialUpdate in Order

--- server/frigobar/views/orderView.py
+++ server/frigobar/views/orderView.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import UpdateModelMixin
 from frigobar.models.order import Order
 from frigobar.serializers.orderSerializer import OrderSerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+class OrderPartialUpdateView(GenericAPIView, UpdateModelMixin):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def put (self, request, *arg, **kwargs):
+        return self.partial_update(request, *arg, **kwargs)
