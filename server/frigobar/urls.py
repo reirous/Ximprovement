ddf4b0925c66c347f626a742810b53187b408164
caiocezarq <caio_queiroz09@hotmail.com>
2020-08-10 21:34:31 -0300

    Ajustes nos Gets
    Utilização do django_filters

--- server/frigobar/urls.py
+++ server/frigobar/urls.py
@@ -18,8 +18,6 @@ from django.urls import path, re_path
 from django.conf.urls import include, url
 from frigobar.views.productView import ProductViewSet
 from frigobar.views.orderView import OrderViewSet
-from frigobar.views.orderView import OrderGetUser
-from frigobar.views.orderView import OrderGetDate
 from frigobar.views.itemView import ItemViewSet
 from frigobar.views.userView import RegisterAPI
 from frigobar.views.loginView import LoginAPI
@@ -36,6 +34,4 @@ urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
     path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', knox_views.LogoutView.as_view(), name='logout'),    
-    path("order/user/<user>/", OrderGetUser.as_view(), name='order_user'),
-    path("order/date/<user>/<start>/<end>/", OrderGetDate.as_view(), name='order_date'),
 ]
--- server/frigobar/views/orderView.py
+++ server/frigobar/views/orderView.py
@@ -1,28 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets, generics
-from rest_framework.generics import ListAPIView
-from rest_framework.decorators import action
-from rest_framework.response import Response
 from frigobar.models.order import Order
 from frigobar.serializers.orderSerializer import OrderSerializer
+from django_filters import rest_framework as filters
+
+class OrderFilters(filters.FilterSet):
+
+    class Meta:
+        model = Order
+        fields = {
+            'user':['exact',],
+            'date': ['lte','gte']}
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-
-class OrderGetUser(ListAPIView):    
-    serializer_class = OrderSerializer
-
-    def get_queryset (self):
-        user = self.kwargs.get('user')
-        return Order.objects.filter(user=user)
-
-
-class OrderGetDate(ListAPIView):
-    serializer_class = OrderSerializer
-    
-    def get_queryset (self):
-        user = self.kwargs.get('user', None)
-        start = self.kwargs.get('start', None)
-        end = self.kwargs.get('end', None)
-        return Order.objects.filter(user=user, date__range=[start, end])
+    filterset_class = OrderFilters
