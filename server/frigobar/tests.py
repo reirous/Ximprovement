75c74bdb9fcecc168d6f0beabe23a685022cf3e8
reirous <luanof@hotmail.com>
2020-08-31 20:04:00 -0300

    Adicionando teste

--- server/frigobar/tests.py
+++ server/frigobar/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from datetime import date
 
-# Create your tests here.
+from frigobar.views.productView import ProductViewSet
+from frigobar.models.product import Product
+
+
+class ProductsUrlsTestCase(TestCase):
+
+    def test_product_create(self):
+        product = Product.objects.create(
+            description="Coca Cola", status=True, price=5, priceDate=date.today(), barCode="1234567890123"
+        )
+
+        product_result = Product.objects.last()
+
+        self.assertEqual(product_result.description, "Coca Cola")
