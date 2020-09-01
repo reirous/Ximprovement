1a911ecca5d5a6df495f5d0f29ea26533c682ad4
reirous <luanof@hotmail.com>
2020-08-31 21:07:31 -0300

    Tentativa de testes

--- server/frigobar/tests.py
+++ server/frigobar/tests.py
@@ -16,3 +16,29 @@ class ProductsUrlsTestCase(TestCase):
         product_result = Product.objects.last()
 
         self.assertEqual(product_result.description, "Coca Cola")
+
+    def test_resolves_list_url(self):
+        resolver = self.resolve_by_name('produtos')
+
+        self.assertEqual(resolver.func.cls, ProductViewSet)
+
+    def test_resolves_retrieve_url(self):
+        resolver = self.resolve_by_name('produtos', pk=1)
+
+        self.assertEqual(resolver.func.cls, ProductViewSet)
+
+    def test_resolves_url_to_list_action(self):
+        resolver = self.resolve_by_name('produtos')
+
+        self.assertIn('get', resolver.func.actions)
+        self.assertEqual('list', resolver.func.actions['get'])
+
+    def test_resolves_url_to_retrieve_action(self):
+        resolver = self.resolve_by_name('produtos', pk=1)
+
+        self.assertIn('get', resolver.func.actions)
+        self.assertEqual('retrieve', resolver.func.actions['get'])
+
+    def resolve_by_name(self, name, **kwargs):
+        url = reverse(name, kwargs=kwargs)
+        return resolve(url)
